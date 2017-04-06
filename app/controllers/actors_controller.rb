d90fa1797cf5f34557dab8930b4c33bec6e6a334
osboxes.org <osboxes.henrygarcia0309@gmail.com>
2017-04-06 12:50:17 -0500

    Add actors

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,5 +1,24 @@
 class ActorsController < ApplicationController
+
   def index
     @actors = Actor.all
+  end
+
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+          redirect_to "/actors"
+    else
+      @errors = @user.errors.full_messages
+      render 'actors/new'
+    end
+  end
+
+  def actor_params
+    params.require(:actor).permit(:name, :bio, :birth_date, :birth_place, :image_url, :alive, :death_date, :death_place)
   end
 end
